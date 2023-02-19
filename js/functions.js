626a3831ed4da3d507f124fb647156fc6e978a26
Anatoly <kolganovpix@yandex.ru>
2023-02-19 20:26:31 +0500

    Добавляет функцию проверки строки на палиндром isStringPalindrom

--- js/functions.js
+++ js/functions.js
@@ -1,2 +1,15 @@
 const checkStringLength = (string, requiredLength) => string.length <= requiredLength;
 
+const isStringPalindrome = (string) => {
+  string = string.toLowerCase();
+  string = string.replaceAll(' ','');
+  let index = 0;
+  for(let i = string.length; i >= 0; i--){
+    if(string[index] === string[i - 1]){
+      index++;
+    }else{
+      return false;
+    }
+  }
+  return true;
+};
