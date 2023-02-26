0060ec635c63a3f891123d18d31d66727bb1f629
Anatoly <kolganovpix@yandex.ru>
2023-02-27 01:12:21 +0500

    Добавляет текст для комментариев

--- js/main.js
+++ js/main.js
@@ -97,6 +97,13 @@ const DESCRIPTION_SECOND_WORD = [
   'Кадр',
 ];
 
+const COMMENTS = `Всё отлично!
+В целом всё неплохо. Но не всё.
+Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.
+Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.
+Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.
+Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!`;
+
 const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
@@ -105,3 +112,4 @@ const getRandomInteger = (a, b) => {
 };
 
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
