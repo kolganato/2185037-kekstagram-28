def9f1a909c99b966649d7a5eeea2d8b04053f0e
Anatoly <kolganovpix@yandex.ru>
2023-03-26 17:41:12 +0500

    Добавляет модули, pristinem nouislider

--- js/render-gallery.js
+++ js/render-gallery.js
@@ -5,9 +5,9 @@ const picturesElement = document.querySelector('.pictures');
 
 const renderGallery = (pictures) => {
   picturesElement.addEventListener('click', (evt) => {
-    evt.preventDefault();
     const picture = evt.target.closest('.picture');
     if(picture){
+      evt.preventDefault();
       const pictureId = Number(picture.dataset.id);
       const pictureData = pictures.find((elem) => elem.id === pictureId);
       openBigPictureModal(pictureData);
